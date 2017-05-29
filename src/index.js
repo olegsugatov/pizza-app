8c83464bbea5a6cd123e8ce4c16cc5cc39790760
olegsugatov <oleg.sugatov@gmail.com>
2017-05-29 17:46:48 +0300

    Header

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
+
+// components
+import { Header } from './components/common';
 
 class App extends Component {
 	render() {
-		const { viewStyle, textStyle } = styles;
 		return (
-			<View style={ viewStyle }>
-				<Text style = { textStyle }>Hello, React Natice!</Text>
+			<View>
+				<Header>
+					Pizza App
+				</Header>
 			</View>
 		);
 	}
 }
-
-const styles = StyleSheet.create({
-	viewStyle: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		flex: 1
-	},
-	textStyle: {
-		fontSize: 18,
-		color: '#383838',
-		fontWeight: '600'
-	}
-});
 
 export default App;
